2cc4b9afe35b1936961187b5449c3f0cab1e3bf8
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2023-11-17 16:45:51 +0000

    25 window

---
+++ best_ckpt_win25/save_model.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+import tensorflow as tf
+import sys
+import argparse
+import glob
+
+from ml.config import cfg
+from ml.dataset import odgt2data
+from ml.models import ModelBuilder
+
+print("All packages imported!")
+
+data = glob.glob('*')
+
+for d in data:
+    if d == 'save_model.py' or d == 'temp_model':
+        continue
+    print(d)
+    config_file = d + '/config.yaml'
+    model_file = d + '/model.tflite'
+    
+    cfg.merge_from_file(config_file)
+    model = ModelBuilder.build_classifier(cfg.MODEL, '', cfg.DATASET.num_classes)
+    if cfg.MODEL.ARCH.LSTM.num_layers > 0:
+      	model.build((1,) + (25, 6))  
+    else:
+    	model.build((None,) + (25, 6))
+    model.save('temp_model')
+    model.summary()
+    converter = tf.lite.TFLiteConverter.from_saved_model('temp_model')
+    
+    converter.target_spec.supported_ops = [
+  		tf.lite.OpsSet.TFLITE_BUILTINS, # enable TensorFlow Lite ops.
+  		tf.lite.OpsSet.SELECT_TF_OPS # enable TensorFlow ops.
+		]
+    converter.experimental_new_converter=True
+    converter.allow_custom_ops=True
+    
+    tflite_model = converter.convert()
+    
+    with open(model_file, 'wb') as f:
+    	f.write(tflite_model)
+    	
