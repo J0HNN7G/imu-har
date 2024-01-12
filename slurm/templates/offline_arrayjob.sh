51b751941bcace111f96c490ea0995faac1206e7
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2024-01-12 19:44:13 +0000

    updated arrayjob

--- slurm/templates/offline_arrayjob.sh
+++ slurm/templates/offline_arrayjob.sh
@@ -197,7 +197,9 @@ echo "Moving output data back to DFS"
 EXP_NAME=${VALS[$LEN - 2]}
 src_path=${SCRATCH_PROJECT_PATH}/${OUTPUT_DN}/${EXP_NAME}
 dest_path=${MAIN_PROJECT_PATH}/${OUTPUT_DN}/${EXP_NAME}
-rsync --archive --update --compress --progress ${src_path}/ ${dest_path}/
+
+mkdir -p ${dest_path}  # make it if required
+rsync --archive --update --compress --progress ${src_path}/ ${dest_path}
 
 
 # ======================================
